cfd95b21f51b88958f11651821e7f505915311c4
hsgill3106 <108752326+hsgill3106@users.noreply.github.com>
2022-07-05 12:14:48 -0600

    js code added

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("hi");
+// SIMPLE ADDITION CALCULATOR
+
+//ADD EVENT LISTENER TO THE BUTTON
+document.getElementById("btn").addEventListener("click", calcTotal);
+
+//EVENT FUNCTION
+function calcTotal() {
+    //INPUT
+let num1 = +document.getElementById("num1").value;
+let num2 = +document.getElementById("num2").value;
+
+    //PROCESS
+let total = num1 + num2;
+
+    //OUTPUT
+    document.getElementById("total").innerHTML = total;
+}
